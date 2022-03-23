db877480c72af153eea8995077af3d1c69853663
mariometzgerv <mariometzgerv@gmail.com>
2022-03-23 12:04:34 -0300

    initial commit

---
+++ estudiante.php
@@ -0,0 +1,19 @@
+<?php
+
+include('db.php');
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
+header('Allow: GET, POST, OPTIONS, PUT, DELETE');
+
+if (isset($_GET['id']))
+{
+	$query = 'CALL `Obtener_Estudiante`(' . $_GET['id'] . ');';
+	$result = mysqli_query($conn, $query);
+	$data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+	
+	echo json_encode($data);
+}
+
+?>
--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<p>Kimen Online Backend v0.1.0</p>
+<p>Kimen Online Backend v0.1.1</p>
